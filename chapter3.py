16531d676e3ffe60e70b7735a309a71b53f1c81e
LawrenceCha <lawrencecha93@gmail.com>
2024-04-18 22:03:59 +0900

    day4

--- chapter3.py
+++ chapter3.py
@@ -1,10 +1,29 @@
-winner = 10
+# winner = 10
 
-if winner < 10:
-    print("Run!")
+# if winner < 10:
+#     print("Run!")
 
-elif winner == 10:
-    print("Run!Run!")
+# elif winner == 10:
+#     print("Run!Run!")
 
-else:
-    print("Stop!")
+# else:
+#     print("Stop!")
+
+from random import randint
+
+print("Welcome to Python Casino")
+pc_choice = randint(1, 100)
+
+playing = True
+
+while playing:
+    user_choice = int(input("Enter your guess (1 - 100): "))
+    if user_choice == pc_choice:    
+        print("You win!")
+        playing = False
+    elif user_choice < pc_choice:
+        print("Too low!")
+    elif user_choice > pc_choice:
+        print("Too high!")
+    
+
